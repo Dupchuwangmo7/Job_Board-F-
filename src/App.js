16c464557126a916b52f402d5a14e4003e4c433b
Dupchu Wangmo <02230282.cst@rub.edu.bt>
2024-12-13 15:47:06 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomePage from './Pages/HomePage'; // Adjust the path based on where HomePage.js is located
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
